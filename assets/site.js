9674a702911dc23be06692f335498c5a0b12f467
FranklinDM <mrmineshafter17@gmail.com>
2020-06-07 22:37:31 +0800

    Further changes

--- assets/site.js
+++ assets/site.js
@@ -8,43 +8,17 @@ var Site = {
         return document.getElementById("navigation");
     },
     
-    getTimer: function() {
-        return document.getElementById("timer");
-    },
-    
     initialize: function() {
         let menuButton = Site.getMenuButton();
         if (menuButton) {
             menuButton.addEventListener("click", Site.toggleMenu);
-            Site.getMenu().setAttribute("hide-menu", "true");
-        }
-        if (Site.getTimer()) {
-            Site.updateTime();
-            window.setInterval(Site.updateTime, 1000);
         }
     },
     
     toggleMenu: function() {
-        if (Site.getMenu().getAttribute("state") == "hidden") {
-            Site.getMenu().setAttribute("state", "active");
-        } else {
-            Site.getMenu().setAttribute("state", "hidden");
-        }
+        Site.getMenu().classList.toggle("hidden");
     },
     
-    updateTime: function() {
-        let date = new Date(Date.now());
-        let options = {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-            hour12: true
-        };
-        Site.getTimer().innerText = date.toLocaleString('en-US', options);
-    },
 };
 
 window.addEventListener("DOMContentLoaded", Site.initialize);
